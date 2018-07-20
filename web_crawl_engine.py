e0667c1246e2818dab492ba9bf2a0b0d3313c844
sholmiw <prin1238@gmail.com>
2018-07-20 08:46:58 +0100

    Update web_crawl_engine.py

--- web_crawl_engine.py
+++ web_crawl_engine.py
@@ -67,3 +67,21 @@ def add_page_to_index(index, url, content):
     words = content.split()
     for word in words:
         add_to_index(index, word, url)
+
+def compute_ranks(graph):
+    d = 0.8 # damping factor
+    numloops = 10
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages   
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for node in graph:
+                if page in graph[node]:
+                    newrank = newrank + d * (ranks[node] / len(graph[node]))
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
